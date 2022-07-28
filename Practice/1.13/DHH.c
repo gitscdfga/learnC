02ec899da0e7704ab6e5da742e5d751a1905b145
gitscdfga <1258406202@qq.com>
2022-07-28 22:27:53 +0800

    test1

---
+++ Practice/1.13/DHH.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define MAXHIST 15
+#define MAXWORD 11
+#define IN 1
+#define OUT 0
+//test
+int main (void)
+{
+	int c, nc, i, state;
+	int len;
+	int ovflow;
+	int maxvalue;
+	int wl[MAXWORD];
+
+	ovflow = 0;
+	nc = 0;
+	state = OUT;
+	for (i = 1; i < MAXWORD; ++i)
+		wl[i] = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == '\n'){
+			state = OUT;
+			if (nc > 0)
+				if (nc < MAXWORD)
+					++wl[nc];
+				else
+					++ovflow;
+			nc = 0;	
+		} else if (state == OUT){
+			state = IN;
+			nc = 1;
+		} else
+			++nc;
+	}
+	maxvalue = 0;
+	for (i = 1; i < MAXWORD; ++i)
+		if (wl[i] > maxvalue)
+			maxvalue = wl[i];
+
+	for (i = 1; i < MAXWORD; ++i){
+		printf("%5d - %5d : ", i, wl[i]);
+		if (wl[i] > 0){
+			if (len = wl[i] * MAXHIST / maxvalue <= 0){
+				len = 1;
+			}
+		} else
+			len = 0;
+		while (len > 0){
+			putchar('*');
+			--len;
+		}
+		putchar('\n');
+	}
+	if (ovflow > 0)
+		printf("There are %d words >= %d\n", ovflow, MAXWORD);
+}
