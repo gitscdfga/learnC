a274c3cfbead33ac9f1785298f5599eff75d0866
gitscdfga <1258406202@qq.com>
2022-08-11 19:41:04 +0800

    complete

--- Practice/1.14/DHF_origin.c
+++ Practice/1.14/DHF_origin.c
@@ -6,12 +6,14 @@ int main (void)
 	int c, i, j;
 	int fre[BIGCHAR];
 	int maxvalue;
-	for (i = 97; i < BIGCHAR; ++i)
+	for (i = 97; i < BIGCHAR; ++i){
 		fre[i] = 0;
+	}
 	while ((c = getchar()) != EOF) {
 		for (i = 97; i < BIGCHAR; ++i){
-			if (c == i)
+			if (c == i){
 				++fre[i];
+			}
 		}
 	}
 	maxvalue = 0;
@@ -19,11 +21,13 @@ int main (void)
 		if (maxvalue < fre[i])
 			maxvalue = fre[i];
 	for (i = MAXHIST; i > 0; --i) {
-		for (j = 22; j < BIGCHAR; ++j){
-			if ((fre[j] * MAXHIST / maxvalue) < i)
-				printf("  ");
-			else
+		for (j = 97; j < BIGCHAR; ++j){
+			if ((fre[j] * MAXHIST / maxvalue) >= i){
 				printf(" *");
+			}
+			else{
+				printf("  ");
+			}
 		}
 		putchar('\n');
 	}
