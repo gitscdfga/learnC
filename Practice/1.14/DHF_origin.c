c28cd4fe6a31daee562614c98cd188d76d5ff887
gitscdfga <1258406202@qq.com>
2022-08-14 14:03:49 +0800

    do away with magic numbers

--- Practice/1.14/DHF_origin.c
+++ Practice/1.14/DHF_origin.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-#define BIGCHAR 123
+#define MAXCHAR 123
 #define MAXHIST 15
+#define MINCHAR 97
 int main (void)
 {
 	int c, i, j;
-	int fre[BIGCHAR];
+	int fre[MAXCHAR];
 	int maxvalue;
-	for (i = 97; i < BIGCHAR; ++i){
+	for (i = MINCHAR; i < MAXCHAR; ++i){
 		fre[i] = 0;
 	}
 	while ((c = getchar()) != EOF) {
-		for (i = 97; i < BIGCHAR; ++i){
+		for (i = MINCHAR; i < MAXCHAR; ++i){
 			if (c == i){
 				++fre[i];
 			}
 		}
 	}
 	maxvalue = 0;
-	for (i = 97; i < BIGCHAR; ++i)
+	for (i = MINCHAR; i < MAXCHAR; ++i)
 		if (maxvalue < fre[i])
 			maxvalue = fre[i];
 	for (i = MAXHIST; i > 0; --i) {
-		for (j = 97; j < BIGCHAR; ++j){
+		for (j = MINCHAR; j < MAXCHAR; ++j){
 			if ((fre[j] * MAXHIST / maxvalue) >= i){
 				printf(" *");
 			}
@@ -31,7 +32,7 @@ int main (void)
 		}
 		putchar('\n');
 	}
-	for (i = 97; i < BIGCHAR; ++i){
+	for (i = MINCHAR; i < MAXCHAR; ++i){
 		putchar(' ');
 		putchar(i);
 	}
