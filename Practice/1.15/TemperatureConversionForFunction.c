567a3e20c3bc3f47c54a7617c959cf8df0c2ae74
gitscdfga <1258406202@qq.com>
2022-08-18 01:02:55 +0800

    complete

--- Practice/1.15/TemperatureConversionForFunction.c
+++ Practice/1.15/TemperatureConversionForFunction.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #define UPPER 300
+#define STEP 20
+#define LOWER 0
 /* print Fahrenhiet-Celsius table
  * for fahr = 0, 20, ..., 300 */
 int fahrconversion(int n);
@@ -7,7 +9,7 @@ int fahrconversion(int n);
 int main (void)
 {
 	int fahr;
-   for (fahr = 0; fahr <= UPPER; fahr += 20)
+   for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
 	  printf("%5d %5d\n", fahr, fahrconversion(fahr)); 
    return 0;
 }
