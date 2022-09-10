c337fcfd77734ffc308926c4e95a7f03a5a8e52a
gitscdfga <1258406202@qq.com>
2022-09-10 17:56:22 +0800

    origin program

---
+++ Practice/1.18/RTBT_origin.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define MAXLINE 1000
+int newgetline(char line[], int maxvalue);
+void copy(char to[], char from[]);
+
+/* Remove trailing blanks and tabs from each line of input, and to delete entirely blank lines. */
+int main(void)
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while ((len = newgetline(line, MAXLINE)) > 0)
+		if (len > max){
+			max = len;
+			copy(longest, line);
+		}
+	if (max > 0)
+		printf("%s", longest);
+	return 0;
+}
+
+/* newgetline: get line in s and retrun lengths */
+int newgetline(char s[], lim)
+{
+	int c, i;
+	for (i = 0; i < lim -1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* copy: copy 'from' into 'to'; assum 'to' is big enough */
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
